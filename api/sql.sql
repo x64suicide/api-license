6aca497351c9a785c14bae8c8e35026c1ee834a8
x64bulka <111202920+x64bulka@users.noreply.github.com>
2022-09-11 16:37:59 +0200

    Update sql.sql

--- api/sql.sql
+++ api/sql.sql
@@ -1,4 +1,4 @@
 CREATE TABLE `licenses` (
 	`license` VARCHAR(18) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`expire` DATE NOT NULL
-);
+	`expire` VARCHAR(10) NOT NULL DEFAULT '0000-00-00' COLLATE 'utf8mb4_general_ci'
+)
